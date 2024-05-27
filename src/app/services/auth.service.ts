cc2d21c738c4f57f182c8165824843ffc669c463
Leah Deshar <leah.desar@gmail.com>
2024-05-27 18:12:38 +0545

    auth services --OAuth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private supabase!: SupabaseClient;
 
-  constructor() { }
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+
+    this.supabase.auth.onAuthStateChange((event, session) => {
+      console.log('event', event);
+      console.log('session', session);
+    });
+  }
+
+  async signInWithGoogle() {
+    await this.supabase.auth.signInWithOAuth({
+      provider: 'google',
+    });
+  }
+  async signOut() {
+    await this.supabase.auth.signOut();
+  }
 }
