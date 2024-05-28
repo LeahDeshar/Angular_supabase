c9d4d59058fe522b0a45ab19fd6df28089a7cb5f
Leah Deshar <leah.desar@gmail.com>
2024-05-28 17:31:15 +0545

    supabase services

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,23 +1,51 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ChatService } from '../../supabase/chat.service';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css',
 })
 export class ChatComponent {
   private auth = inject(AuthService);
-  private router = inject(Router);
+  private chat_service = inject(ChatService);
 
+  private router = inject(Router);
+  private fd = inject(FormBuilder);
+  chatForm!: FormGroup;
+
+  constructor() {
+    this.chatForm = this.fd.group({
+      chat_message: ['', Validators.required],
+    });
+  }
   async logout() {
     this.auth
       .signOut()
       .then(() => {
         this.router.navigate(['/login']);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }
+
+  onSubmit() {
+    const formValue = this.chatForm.value.chat_message;
+    this.chat_service
+      .chatMessage(formValue)
+      .then(() => {
+        this.chatForm.reset();
       })
       .catch((err) => {
         alert(err.message);
--- src/app/supabase/chat.service.ts
+++ src/app/supabase/chat.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
+  private supabase!: SupabaseClient;
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+  }
 
-  constructor() { }
+  async chatMessage(text: string) {
+    try {
+      const { data, error } = await this.supabase.from('chat').insert({ text });
+
+      if (error) {
+        alert(error.message);
+      }
+      return data;
+    } catch (error) {
+      alert(error);
+    }
+  }
 }
