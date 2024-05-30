0332ea6e60da7fbbfd30a86fe75e1b0ee51b34fe
Leah Deshar <leah.desar@gmail.com>
2024-05-30 15:23:29 +0545

    chat modal

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://dndibgauezhpheyknzza.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuZGliZ2F1ZXpocGhleWtuenphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzMTI4NjgsImV4cCI6MjAzMTg4ODg2OH0.Z4l4RnIHXBRCPBamqld6JF47PaWXFUXlDO3qxhOVxE8',
+};
